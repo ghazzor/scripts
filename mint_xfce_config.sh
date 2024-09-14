4bce763409d4421fba97cbc25187cf2137cd1e2a
ghazzor <kartikhis8o@gmail.com>
2024-09-14 13:15:29 +0530

    mint_xfce_config: add wireguard script and some shell macros

--- mint_xfce_config.sh
+++ mint_xfce_config.sh
@@ -3,6 +3,26 @@
 # dirs
 mkdir -p $HOME/android
 
+# move scripts and services
+chmod 755 wgs
+sudo cp wgs /usr/local/bin/wgs
+sudo chown root:root /usr/local/bin/wgs
+
+ 
+shellrc="$HOME/.zshrc"
+shell_rc_lines=(
+'export PATH=$HOME/.bin:$PATH'
+'source $HOME/android/source'
+'export MICRO_TRUECOLOR=1'
+)
+
+# Iterate over each line in the array
+for line in "${shell_rc_lines[@]}"; do
+  if ! grep -Fxq "$line" "$shellrc"; then
+  echo "$line" >> "$shellrc"
+fi
+done
+
 # configs
 #sudo update-alternatives --config x-cursor-theme #to select default cursor theme systemwide
 sudo update-alternatives --set x-cursor-theme /usr/share/icons/DMZ-White/cursor.theme # use DMZ-white as default
